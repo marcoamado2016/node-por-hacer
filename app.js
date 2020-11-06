a54826d92e53bba4bdcae03a71ece652608a407f
Marco Amado <marco.amado@ross.com.ar>
2020-11-06 20:48:22 -0300

    segundo commit

--- app.js
+++ app.js
@@ -31,6 +31,7 @@ switch(comando){
   break;
   default:
        console.log('--Comando no reconocido--');
+       console.log();
 
 }
 
@@ -40,11 +41,19 @@ switch(comando){
 ### Aplicacion de comandos
 Este es el ejercicio del curso
 
-recuerden los paquetes de node
+recuerden los paquetes de node js
 ````
 npm install
 ````
 3-crear el archivo que contiene lo que no quiero subir en github => .gitignore
 4-inicializar el repositorio => git init
 5-git status para ver los archivo que son candidatos a ser subidos.
+6-git add. para agregar al area de stage
+7-git status ver un cambio por que ahora los archivos estan en el area de stage
+8-git commit -m "Primer confirmacion"
+9 git remote add origin https://github.com/marcoamado2016/node-por-hacer.git
+10-git push -u origin master => empujar el proyecto al repositorio
+11-git tag -a v1.0.0 -m "Primera version"
+12-git tag
+13-git push --tags
 */
